bec7e4433c21289c919d6e65ee64357410669f6e
michaelgorman18@gmail.com <michaelgorman18@gmail.com>
2019-06-05 20:01:59 +1000

    move logic into class

--- main.py
+++ main.py
@@ -2,10 +2,43 @@ import json
 import sys
 
 class Node():
-	def __init__(self, fingers, barre):
+	def __init__(self, frets, fingers, barre):
+		self.frets = frets
 		self.fingers = fingers
 		self.barre = barre
-		
+		self.finger_chart = self.generate_finger_chart(frets, fingers)
+	
+	def generate_finger_chart(self, fingers, barre):
+		data = []
+	
+		for fret in range(base_fret, base_fret+6):
+			data.append([])
+			for string in range(1,7):
+				if frets[string-1] == fret and fingers[string-1] != -1:
+					data[fret-base_fret].append(fingers[string-1])
+				else:
+					data[fret-base_fret].append(-1)
+					
+	def print_chord(self):
+		base_fret = min((f for f in self.frets if f > 0))
+		for j in "EADGBe":
+			sys.stdout.write(j + ("  " * int(j != 'e')))
+		sys.stdout.write("\n")
+		for j in range(0, 6):
+			sys.stdout.write("|" + ("==" * int(j != 5)))
+
+		for j in range(base_fret, base_fret + 6):
+			sys.stdout.write("\n")
+			for i in range(1, 7):
+				symbol = "|"
+				if frets[i-1] == j and fingers[i-1] != 0:
+					symbol = str(fingers[i-1])
+				elif frets[i-1] == -1 and j == base_fret:
+					symbol = "x"
+				sys.stdout.write(symbol + ("--" * int(i != 6)))
+			if (j == base_fret):
+				sys.stdout.write("  " + str(base_fret))
+
 
 chords = {}
 
@@ -20,52 +53,11 @@ data = json.loads(open("guitar.json").read())
 		print suffix
 """
 	
-def print_chord(chord):
-	frets = [0, 2, 2, 1, 0, 0]
-	fingers = [-1, 3, 4, 1, -1, -1]
-	base_fret = min((f for f in frets if f > 0))
+frets = [-1, 7, 9, 9, 9, 7]
+fingers = [-1, 1, 3, 3, 3, 1]
+base_fret = min((f for f in frets if f > 0))
 
-	data = []
-	
-	for fret in range(base_fret, base_fret+6):
-		data.append([])
-		for string in range(1,7):
-			if frets[string-1] == fret and fingers[string-1] != -1:
-				data[fret-base_fret].append(fingers[string-1])
-			else:
-				data[fret-base_fret].append(-1)
-	
-	for i in data:
-		print i
-			
-	1/0
-	
-	
-	print base_fret 
-	
-	for j in "ADGDBe":
-		sys.stdout.write(j + ("  " * int(j != 'e')))
-	sys.stdout.write("\n")
-	for j in range(0, 6):
-		sys.stdout.write("|" + ("==" * int(j != 5)))
+chord = Node([-1, 7, 9, 9, 9, 7], [-1, 1, 3, 3, 3, 1], 2)
 
-	for j in range(base_fret, base_fret + 6):
-		sys.stdout.write("\n")
-		for i in range(1, 7):
-			symbol = "|"
-			if frets[i-1] == j and fingers[i-1] != 0:
-				symbol = str(fingers[i-1])
-			sys.stdout.write(symbol + ("--" * int(i != 6)))
-		if (j == base_fret):
-			sys.stdout.write("  5")
-
-
-		#sys.stdout.write("\n")
-		#for i in range(0, 6):
-			#if (j < 5):
-				#sys.stdout.write("|" + ("  " * int(i != 5)))
-	
-	
-	
-print_chord(1)
+chord.print_chord()
 	
