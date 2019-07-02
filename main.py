4e88401a77739066e8d9337bf4a55ac4e910f81f
U-MICHAEL\Michael <michaelgorman18@gmail.com>
2019-07-02 18:45:03 +1000

    Load chords

--- main.py
+++ main.py
@@ -1,63 +1,65 @@
 import json
 import sys
 
-class Node():
-	def __init__(self, frets, fingers, barre):
+class Chord():
+	def __init__(self, frets, fingers):
 		self.frets = frets
 		self.fingers = fingers
-		self.barre = barre
+		self.base_fret = min((f for f in frets if f > 0)) 
 		self.finger_chart = self.generate_finger_chart(frets, fingers)
 	
-	def generate_finger_chart(self, fingers, barre):
+	def generate_finger_chart(self, frets, fingers):
 		data = []
 	
-		for fret in range(base_fret, base_fret+6):
+		for fret in range(self.base_fret, self.base_fret+6):
 			data.append([])
 			for string in range(1,7):
 				if frets[string-1] == fret and fingers[string-1] != -1:
-					data[fret-base_fret].append(fingers[string-1])
+					data[fret-self.base_fret].append(fingers[string-1])
 				else:
-					data[fret-base_fret].append(-1)
+					data[fret-self.base_fret].append(-1)
 					
 	def print_chord(self):
-		base_fret = min((f for f in self.frets if f > 0))
 		for j in "EADGBe":
 			sys.stdout.write(j + ("  " * int(j != 'e')))
 		sys.stdout.write("\n")
 		for j in range(0, 6):
 			sys.stdout.write("|" + ("==" * int(j != 5)))
-
-		for j in range(base_fret, base_fret + 6):
+		for j in range(self.base_fret-2, self.base_fret + 6):
 			sys.stdout.write("\n")
 			for i in range(1, 7):
 				symbol = "|"
-				if frets[i-1] == j and fingers[i-1] != 0:
-					symbol = str(fingers[i-1])
-				elif frets[i-1] == -1 and j == base_fret:
+				if self.frets[i-1] == j and self.fingers[i-1] != 0:
+					symbol = str(self.fingers[i-1])
+				elif self.frets[i-1] == -1 and j == self.base_fret:
 					symbol = "x"
 				sys.stdout.write(symbol + ("--" * int(i != 6)))
-			if (j == base_fret):
-				sys.stdout.write("  " + str(base_fret))
+			if (j == self.base_fret):
+				sys.stdout.write("  " + str(self.base_fret))
+				
+		sys.stdout.write("\n" * 2)
 
 
-chords = {}
+chord_dict = {}
 
 data = json.loads(open("guitar.json").read())
 
-"""for chord in data["chords"]:
-	chord_info = data["chords"][chord]
-	for chord_suffix in chord_info:
-		suffix = chord_suffix["suffix"]
+for chord_name in data["chords"]:
+	chords = data["chords"][chord_name]
+	for chord in chords:
+		suffix = chord["suffix"]
 		if suffix != "major" and suffix != "minor":
 			continue
-		print suffix
-"""
-	
-frets = [-1, 7, 9, 9, 9, 7]
-fingers = [-1, 1, 3, 3, 3, 1]
-base_fret = min((f for f in frets if f > 0))
+		name = chord_name + suffix
+		for position in chord["positions"]:
+			frets = position["frets"]
+			fingers = position["fingers"]
+			if name not in chord_dict:
+				chord_dict[name] = []
+			chord_dict[name].append(Chord(frets, fingers))
+			
+		
 
-chord = Node([-1, 7, 9, 9, 9, 7], [-1, 1, 3, 3, 3, 1], 2)
-
-chord.print_chord()
+for chord in chord_dict["Emajor"]:
+	chord.print_chord()
 	
